39cd102d87d14b2dcc86e732738b88f538684cd7
plusoneky <plusone@163.com>
2018-01-23 16:59:47 +0800

    test

--- src/main/java/com/qq1833111108/config/shiro/MyKickoutSessionControlFilter.java
+++ src/main/java/com/qq1833111108/config/shiro/MyKickoutSessionControlFilter.java
@@ -34,120 +34,119 @@ import java.util.LinkedList;
 import java.util.Map;
 
 public class MyKickoutSessionControlFilter extends AccessControlFilter {
-	
+
 	private static final Logger logger = LoggerFactory.getLogger(MyKickoutSessionControlFilter.class);
 
-    private String kickoutUrl; //踢出后到的地址
-    private boolean kickoutAfter = false; //踢出之前登录的/之后登录的用户 默认踢出之前登录的用户
-    private int maxSession = 1; //同一个帐号最大会话数 默认1
+	private String kickoutUrl; // 踢出后到的地址
+	private boolean kickoutAfter = false; // 踢出之前登录的/之后登录的用户 默认踢出之前登录的用户
+	private int maxSession = 1; // 同一个帐号最大会话数 默认1
 
-    private SessionManager sessionManager;
-    private Cache<String, Deque<Serializable>> cache;
-    
-    private static final String SHIRO_KICKOUT_SESSION_KEY_NAME = "shiro-kickout-session";
+	private SessionManager sessionManager;
+	private Cache<String, Deque<Serializable>> cache;
 
-    public void setKickoutUrl(String kickoutUrl) {
-        this.kickoutUrl = kickoutUrl;
-    }
+	private static final String SHIRO_KICKOUT_SESSION_KEY_NAME = "shiro-kickout-session";
 
-    public void setKickoutAfter(boolean kickoutAfter) {
-        this.kickoutAfter = kickoutAfter;
-    }
+	public void setKickoutUrl(String kickoutUrl) {
+		this.kickoutUrl = kickoutUrl;
+	}
 
-    public void setMaxSession(int maxSession) {
-        this.maxSession = maxSession;
-    }
+	public void setKickoutAfter(boolean kickoutAfter) {
+		this.kickoutAfter = kickoutAfter;
+	}
 
-    public void setSessionManager(SessionManager sessionManager) {
-        this.sessionManager = sessionManager;
-    }
+	public void setMaxSession(int maxSession) {
+		this.maxSession = maxSession;
+	}
 
-    //设置Cache的key的前缀
-    public void setCacheManager(CacheManager cacheManager) {
-        this.cache = cacheManager.getCache(SHIRO_KICKOUT_SESSION_KEY_NAME);
-    }
+	public void setSessionManager(SessionManager sessionManager) {
+		this.sessionManager = sessionManager;
+	}
 
-    @Override
-    protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue) throws Exception {
-        return false;
-    }
+	// 设置Cache的key的前缀
+	public void setCacheManager(CacheManager cacheManager) {
+		this.cache = cacheManager.getCache(SHIRO_KICKOUT_SESSION_KEY_NAME);
+	}
 
-    @Override
-    protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
-        Subject subject = getSubject(request, response);
-        if(!subject.isAuthenticated() && !subject.isRemembered()) {
-            //如果没有登录，直接进行之后的流程
-            return true;
-        }
+	@Override
+	protected boolean isAccessAllowed(ServletRequest request, ServletResponse response, Object mappedValue)
+			throws Exception {
+		return false;
+	}
 
-        Session session = subject.getSession();
-        User user = (User) subject.getPrincipal();
-        String username = user.getUsername();
-        
-        Serializable sessionId = session.getId();
+	@Override
+	protected boolean onAccessDenied(ServletRequest request, ServletResponse response) throws Exception {
+		Subject subject = getSubject(request, response);
+		if (!subject.isAuthenticated() && !subject.isRemembered()) {
+			// 如果没有登录，直接进行之后的流程
+			return true;
+		}
 
-        //TODO 同步控制，读取缓存   没有就存入
-        Deque<Serializable> deque = cache.get(username);
-        if(deque == null) {
-        	//将sessionId存入队列
-            deque = new LinkedList<Serializable>();
-            //将用户的sessionId队列缓存
-            cache.put(username, deque);
-        }
+		Session session = subject.getSession();
+		User user = (User) subject.getPrincipal();
+		String username = user.getUsername();
 
-        //如果队列里没有此sessionId，且用户没有被踢出；放入队列
-        if(!deque.contains(sessionId) && session.getAttribute("kickout") == null) {
-            deque.push(sessionId);
-            cache.put(username, deque);
-        }
+		Serializable sessionId = session.getId();
 
-        //如果队列里的sessionId数超出最大会话数，开始踢人
-        while(deque.size() > maxSession) {
-            Serializable kickoutSessionId = null;
-            if(kickoutAfter) { //如果踢出后者
-                kickoutSessionId = deque.removeFirst();
-            } else { //否则踢出前者
-                kickoutSessionId = deque.removeLast();
-            }
-            try {
-                Session kickoutSession = sessionManager.getSession(new DefaultSessionKey(kickoutSessionId));
-                if(kickoutSession != null) {
-                    //设置会话的kickout属性表示踢出了
-                    kickoutSession.setAttribute("kickout", true);
-                }
-            } catch (Exception e) {//ignore exception
-            	//logger.error("",e);
-            }
-        }
+		// TODO 同步控制，读取缓存 没有就存入
+		Deque<Serializable> deque = cache.get(username);
+		if (deque == null) {
+			// 将sessionId存入队列
+			deque = new LinkedList<Serializable>();
+			// 将用户的sessionId队列缓存
+			cache.put(username, deque);
+		}
 
-        //如果被踢出了，直接退出，重定向到踢出后的地址
-        if ((Boolean)session.getAttribute("kickout")!=null&&(Boolean)session.getAttribute("kickout") == true) {
-            //会话被踢出了
-            try {
-                subject.logout();
-            } catch (Exception e) { //ignore
-            	logger.error("",e);
-            }
-            saveRequest(request);
-            
-            
-			//判断是不是Ajax请求
+		// 如果队列里没有此sessionId，且用户没有被踢出；放入队列
+		if (!deque.contains(sessionId) && session.getAttribute("kickout") == null) {
+			deque.push(sessionId);
+			cache.put(username, deque);
+		}
+
+		// 如果队列里的sessionId数超出最大会话数，开始踢人
+		while (deque.size() > maxSession) {
+			Serializable kickoutSessionId = null;
+			if (kickoutAfter) { // 如果踢出后者
+				kickoutSessionId = deque.removeFirst();
+			} else { // 否则踢出前者
+				kickoutSessionId = deque.removeLast();
+			}
+			try {
+				Session kickoutSession = sessionManager.getSession(new DefaultSessionKey(kickoutSessionId));
+				if (kickoutSession != null) {
+					// 设置会话的kickout属性表示踢出了
+					kickoutSession.setAttribute("kickout", true);
+				}
+			} catch (Exception e) {// ignore exception
+				// logger.error("",e);
+			}
+		}
+
+		// 如果被踢出了，直接退出，重定向到踢出后的地址
+		if ((Boolean) session.getAttribute("kickout") != null && (Boolean) session.getAttribute("kickout") == true) {
+			// 会话被踢出了
+			try {
+				subject.logout();
+			} catch (Exception e) { // ignore
+				logger.error("", e);
+			}
+			saveRequest(request);
+
+			// 判断是不是Ajax请求
 			if ("XMLHttpRequest".equalsIgnoreCase(((HttpServletRequest) request).getHeader("X-Requested-With"))) {
-				
+
 				ApiResDto resDto = new ApiResDto(new MyException(CommErrCode.KickOutByAnotherLogin));
-				//输出json串
+				// 输出json串
 				ajaxOut(response, resDto);
-			}else{
-				//重定向
+			} else {
+				// 重定向
 				WebUtils.issueRedirect(request, response, kickoutUrl);
-			}            
-            return false;
-        }
-        return true;
-    }
-    
-    private void ajaxOut(ServletResponse hresponse, ApiResDto resDto)
-			throws IOException {
+			}
+			return false;
+		}
+		return true;
+	}
+
+	private void ajaxOut(ServletResponse hresponse, ApiResDto resDto) throws IOException {
 		try {
 			hresponse.setCharacterEncoding("UTF-8");
 			PrintWriter out = hresponse.getWriter();
@@ -155,30 +154,31 @@ public class MyKickoutSessionControlFilter extends AccessControlFilter {
 			out.flush();
 			out.close();
 		} catch (Exception e) {
-			logger.error("KickoutSessionFilter.class 输出JSON异常，可以忽略。",e);
+			logger.error("KickoutSessionFilter.class 输出JSON异常，可以忽略。", e);
 		}
-	}    
-    
+	}
+
 	@Override
 	protected boolean preHandle(ServletRequest servletRequest, ServletResponse servletResponse) throws Exception {
 		HttpServletRequest request = (HttpServletRequest) servletRequest;
 		HttpServletResponse response = (HttpServletResponse) servletResponse;
-		System.out.println("-----------------Origin-------------------------"+request.getHeader("Origin"));
-		
+		System.out.println("-----------------Origin-------------------------" + request.getHeader("Origin"));
+
 		if (request.getMethod().equals(RequestMethod.OPTIONS.name())) {
-	        response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
-	        response.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,OPTIONS,DELETE");
-	        response.setHeader("Access-Control-Allow-Headers", "origin,x-requested-with,content-type,Accept,Access-Control-Allow-Origin,EX-SysAuthToken,EX-JSESSIONID,Authorization");
-	        response.setHeader("Access-Control-Max-Age", "3628800");
-	        response.setHeader("Access-Control-Allow-Credentials","true");		
-	        
-			System.out.println("request.getRequestURI()="+request.getRequestURI());
-			System.out.println("request.getMethod()="+request.getMethod());			
-			
+			response.setHeader("Access-Control-Allow-Origin", request.getHeader("Origin"));
+			response.setHeader("Access-Control-Allow-Methods", "POST,GET,PUT,OPTIONS,DELETE");
+			response.setHeader("Access-Control-Allow-Headers",
+					"origin,x-requested-with,content-type,Accept,Access-Control-Allow-Origin,EX-SysAuthToken,EX-JSESSIONID,Authorization");
+			response.setHeader("Access-Control-Max-Age", "3628800");
+			response.setHeader("Access-Control-Allow-Credentials", "true");
+
+			System.out.println("request.getRequestURI()=" + request.getRequestURI());
+			System.out.println("request.getMethod()=" + request.getMethod());
+
 			response.setStatus(HttpStatus.OK.value());
 			return false;
 		}
 
 		return super.preHandle(request, response);
-	}    
+	}
 }
