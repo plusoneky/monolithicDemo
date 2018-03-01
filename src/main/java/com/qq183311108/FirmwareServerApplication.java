6e5b9afc94918f463072e4d0d199188885ff7add
lijiabei@renrph.com <lijiabei@renrph.com>
2018-03-01 18:48:43 +0800

    d

--- src/main/java/com/qq183311108/FirmwareServerApplication.java
+++ src/main/java/com/qq183311108/FirmwareServerApplication.java
@@ -1,11 +1,23 @@
 package com.qq183311108;
 
+import javax.servlet.MultipartConfigElement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class FirmwareServerApplication {
 
+	@Bean
+	MultipartConfigElement multipartConfigElement(){
+		MultipartConfigFactory foctory = new MultipartConfigFactory();
+		foctory.setLocation("/tmp");
+		return foctory.createMultipartConfig();
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(FirmwareServerApplication.class, args);
 	}
